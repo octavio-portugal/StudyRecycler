36d98c2c9cca6adf8053e1701009271ebb2327b2
Octavio Portugal <octaviolportugal@gmail.com>
2021-04-11 21:16:23 -0300

    menus created

--- app/src/main/java/com/dio/recyclerviewstudidio/MainActivity.kt
+++ app/src/main/java/com/dio/recyclerviewstudidio/MainActivity.kt
@@ -2,11 +2,15 @@ package com.dio.recyclerviewstudidio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
-    private val rv_list:RecyclerView by lazy {
+    private val rv_list: RecyclerView by lazy {
         findViewById<RecyclerView>(R.id.rv_list)
     }
     private val adapter = ContactAdapter()
@@ -19,25 +23,49 @@ class MainActivity : AppCompatActivity() {
         updatelist()
     }
 
-    private fun bindViews(){
+    private fun bindViews() {
         rv_list.adapter = adapter
         rv_list.layoutManager = LinearLayoutManager(this)
     }
 
-    private fun updatelist(){
+    private fun updatelist() {
         adapter.updateList(
             arrayListOf(
                 Contact(
                     "Octavio Portugal",
                     "(99)8888-8888",
                     "img.png"
-                ) ,
-                        Contact(
-                        "Octavio Portugal",
-                "(99)8888-8888",
-                "img.png"
-            )
+                ),
+                Contact(
+                    "Octavio Portugal",
+                    "(99)8888-8888",
+                    "img.png"
+                )
             )
         )
+    }
+
+    private fun showToas(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.item_menu_1 -> {
+                showToas("Exibindo item de menu 1")
+                true
+            }
+            R.id.item_menu_2 -> {
+                showToas("Exibindo item de menu 2")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
